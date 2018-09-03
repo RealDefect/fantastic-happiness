fb1a50dba3f4264bb93e37aaf72e1d7fcf653313
RealDefect <34613759+RealDefect@users.noreply.github.com>
2018-09-03 00:43:25 -0500

    last push for the night

--- Game.py
+++ Game.py
@@ -13,7 +13,8 @@ bow = 10
 arrow = 4
 torch = 3
 
-
+#need to working on fixing commented out items.
+#also when running in GitBash text does not display asking for user input need to research defect
 
 def front_gate():
 	print "You wake up in a forest lush with green tree's." 
@@ -25,14 +26,14 @@ def front_gate():
             print "It opens slowly to a court yard. So many bushes around you can't count them all."
             print "You look around searching for anyone else."  
             court_yard()
-            else if castlegate == "walk the opposite direction":
-                print "You turn around and start walking into the forest."
-                print "You fall into a pit breaking your leg." 
-	            print "You scream for help but no one comes to your aide."
-                dead()
-                else:
-                    print "You find a place to relax under a tree and fall asleep."
-                    front_gate()
+            #else if castlegate == "walk the opposite direction":
+              #print "You turn around and start walking into the forest."
+               # print "You fall into a pit breaking your leg." 
+	            #print "You scream for help but no one comes to your aide."
+                #dead()
+                #else:
+                  #  print "You find a place to relax under a tree and fall asleep."
+                   # front_gate()
 		
 
 # end game for the user 	
@@ -58,13 +59,13 @@ def green_door():
 	print  "As you get closer to the door it hums.... Strange...."
 	print  "The door says please pick a number 1 through 10."
 	print  "What number do you choose?"
-	door_list = [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+	door_list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 	guessnumber = int(input())
 	if guessnumber in door_list(0,4):
 		print "You are allowed to enter."
 		print "The door opens slowly for you."
 		# need to write what happens next for the user 
-	else guessnumber in door_list(5,9):
+	else:
 		print "You are not allowed to enter." 
 		print "Nothing else happens......."
 		court_yard()
@@ -77,18 +78,16 @@ def blue_door():
 	print "The door opens slowly, to room filled with gold coins."
 	print "How many coins do you take ?" 
 	pockets = int(input())
-	
 	if pockets <= 500:
 		print "You place a handful of coins in your pocket." 
 		print "You quickly shut the door before anyone sees you. "
 		print "You run outside the gate it slams closed behind you."
 		front_gate()
-		
-	else pockets > 500:
-        print "As you are filling your pockets with gold something moves."
-		print "Suddenly a snack leaps out from under the coins bitting you."
-		print "Everything goes dark!!!......."
-		dead()
+	else:
+           #print "As you are filling your pockets with gold something moves."
+		   #print "Suddenly a snack leaps out from under the coins bitting you."
+           #print "Everything goes dark!!!......."
+		   dead()
 
 
 	
