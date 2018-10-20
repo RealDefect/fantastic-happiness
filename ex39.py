b395ba539840c698b60f27a0c6a7efda45af8cb3
RealDefect <34613759+RealDefect@users.noreply.github.com>
2018-10-20 01:08:50 -0500

    starting of exercise 39

---
+++ ex39.py
@@ -0,0 +1,20 @@
+# create a mapping of state to abreviation
+states = {
+    'Orgeron':  'OR'
+    'Florida': 'FL'
+    'California': 'CA'
+    'New York': 'NY'
+    'Michigan': 'MI'
+}
+
+# create a basi set of states and some cities in them 
+cities = {
+    'CA': 'San Franciso',
+    'MI': 'Detroit',
+    'FL': 'Jacksonville',
+}
+
+# addsome more cities 
+cities ['NY'] = 'New York'
+cities ['OR'] = 'Portlan'
+# need to finish writing out code 
