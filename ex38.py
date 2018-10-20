81e43109321d680f370b1190e8ac04b0b7d81e8a
RealDefect <34613759+RealDefect@users.noreply.github.com>
2018-10-19 21:24:32 -0500

    fixing bugs with ex38.py

--- ex38.py
+++ ex38.py
@@ -1,17 +1,17 @@
-ten_things = " Apples Oranges Crows Telephone Light Sugar"
+ten_things = "Apples Oranges Crows Telephone Light Sugar"
 
 print "Wait there's not 10 things in that list, lets fix that."
 
 stuff = ten_things.split(' ')
-more_stuff = ["Day", "Night" , "Song" , "Frisbee" , "Corn" , "Banana" , "Girl" , "Boy"]
+more_stuff = ["Day", "Night" , "Song", "Frisbee", "Corn", "Banana", "Girl", "Boy"]
 
 while len(stuff) != 10:
     next_one = more_stuff.pop()
-    print"Adding: ", next_one
+    print "Adding: ",  next_one
     stuff.append(next_one)
     print "There's %d items now." % len(stuff)
 
-print "There we go: " ,  stuff
+print "There we go: ",  stuff
 
 print "Let's do some thing with stuff."
 
